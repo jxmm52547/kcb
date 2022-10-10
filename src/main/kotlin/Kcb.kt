d86cabcaa94673086e7dbc19a3c4a6ae46f7f562
jxmm52547 <84485811+jxmm52547@users.noreply.github.com>
2022-10-10 11:36:34 +0800

    Add files via upload

---
+++ src/main/kotlin/Kcb.kt
@@ -0,0 +1,23 @@
+package xyz.jxmm
+
+import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
+import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.utils.info
+
+object Kcb : KotlinPlugin(
+    JvmPluginDescription(
+        id = "xyz.jxmm.kcb",
+        name = "课程表",
+        version = "0.1.1",
+    ) {
+        author("靖暄")
+    }
+) {
+    override fun onEnable() {
+        logger.info { "课程表插件加载成功" }
+        main()
+        kotlin.run { xyz.jxmm.getCSC() }
+        time()
+    }
+}
+
